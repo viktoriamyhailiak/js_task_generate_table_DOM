c3258b66900e03e4b2bb8023a64c8b65e0ffa21c
Viktoriia <viktoriamyhailiak@gmail.com>
2025-06-01 21:07:14 +0300

    add a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -359,10 +359,10 @@ const tbody = document.getElementsByTagName('tbody')[0];
 for (const person of people) {
   const row = document.createElement('tr');
   const keys = [
-    person.name,
-    person.sex,
-    person.born,
-    person.died,
+    'name',
+    'sex',
+    'born',
+    'died',
     person.died - person.born,
     Math.ceil(person.died / 100),
   ];
@@ -372,7 +372,12 @@ for (const person of people) {
   for (let i = 0; i < 6; i++) {
     const cell = document.createElement('td');
 
-    cell.innerHTML = keys[i];
-    row.append(cell);
+    if (i >= 4) {
+      cell.innerHTML = keys[i];
+      row.append(cell);
+    } else {
+      cell.innerHTML = person[keys[i]];
+      row.append(cell);
+    }
   }
 }
